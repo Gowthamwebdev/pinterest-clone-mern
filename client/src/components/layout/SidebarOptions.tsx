a5dfc2d41c1eca025878c776cde54a903125b5df
gowtham <gowthamgalaxy007@gmail.com>
2025-04-22 13:59:02 +0530

    refactor(client): createPost, sidebarOptions

--- client/src/components/layout/SidebarOptions.tsx
+++ client/src/components/layout/SidebarOptions.tsx
@@ -1,12 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SidebarOptions = ({ setSetting }: { setSetting: (val: boolean) => void }) => {
   const ref = useRef<HTMLDivElement>(null);
-  useEffect(() => {
+  const [fadeOut, setFadeOut] = useState(false);
+
+  // Outside click logic
+  useEffect(() => { 
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
-        setSetting(false);
+        triggerFadeOut();
       }
     };
 
@@ -14,22 +17,30 @@ const SidebarOptions = ({ setSetting }: { setSetting: (val: boolean) => void }) 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [setSetting]);
+  }, []);
+
+  const triggerFadeOut = () => {
+    setFadeOut(true);
+    setTimeout(() => setSetting(false), 200); 
+  };
+
+  const handleLinkClick = () => {
+    triggerFadeOut();
+  };
 
   return (
     <div
       ref={ref}
-      className="absolute bottom-6 left-24 z-50 w-96 h-inherit bg-white border border-gray-200 rounded-lg shadow-lg p-4 flex flex-col gap-4"
+      className={`absolute bottom-6 left-24 z-50 w-96 h-inherit m-2 bg-white border font-semibold border-gray-200 rounded-lg shadow-lg p-4 flex flex-col gap-4 transition-opacity duration-200 ${
+        fadeOut ? "opacity-0" : "opacity-100"
+      }`}
     >
-    <Link to="/settings" className=" cursor-pointer">Settings</Link>
-    <Link to="/edit/history" className=" cursor-pointer">Edit History</Link>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
-    <a href=""></a>
+      <Link to="/settings" className="cursor-pointer hover:bg-gray-200 p-2 rounded-lg" onClick={handleLinkClick}>
+        Settings
+      </Link>
+      <Link to="/edit/history" className="cursor-pointer hover:bg-gray-200 p-2 rounded-lg " onClick={handleLinkClick}>
+        Edit History
+      </Link>
     </div>
   );
 };
