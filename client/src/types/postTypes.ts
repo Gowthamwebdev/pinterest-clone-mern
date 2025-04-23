d1844b96df857871788a059e5a13d5fe1b468ec9
gowtham <gowthamgalaxy007@gmail.com>
2025-04-23 22:52:51 +0530

    refactor(client): CreatePost(MUI), pinTypes, userTypes

--- client/src/types/postTypes.ts
+++
@@ -1,7 +0,0 @@
-export type postType = {
-    title: string;
-    desc: string;
-    link: string;
-    tag: string;
-  };
-  
---
+++ client/src/types/userTypes.ts
@@ -0,0 +1,6 @@
+export type userType = {
+    user_id: string,
+    name: string,
+    email: string,
+    profile_img: string,
+}
