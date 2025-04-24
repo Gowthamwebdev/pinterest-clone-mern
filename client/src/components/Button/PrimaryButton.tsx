0c6140038a1e5669af0e6d93645643eff1d48793
JoshiyaRavi <joshiyaravi@gmail.com>
2025-04-24 10:04:13 +0530

    Added BaseButton

---
+++ client/src/components/Button/PrimaryButton.tsx
@@ -0,0 +1,26 @@
+
+import { MouseEventHandler, ReactNode } from "react";
+import BaseButton from "./BaseButton";
+
+type PrimaryButton = {
+  className?: string;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  children: ReactNode;
+};
+
+export default function PrimaryButton({
+  className,
+  onClick,
+  children,
+}: PrimaryButton) {
+  return (
+    <BaseButton
+      color="#E60023"
+      textColor="#ffffff"
+      className={`font-bold ${className}`}
+      onClick={onClick}
+    >
+      {children}
+    </BaseButton>
+  );
+}
